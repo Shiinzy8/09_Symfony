ac288b10d077c670c19b25a321191b884e5bb92d
Shiinzy8 <Shiinzy8@gmail.com>
2017-01-09 15:52:42 +0200

    017 views for item for admin

--- sf/src/AppBundle/Controller/Admin/ItemController.php
+++ sf/src/AppBundle/Controller/Admin/ItemController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Admin;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -19,7 +19,7 @@ class ItemController extends Controller
     /**
      * items list page
      *
-     * @Route("/item" , name="item_list")
+     * @Route("/admin/" , name="admin_item_list")
      * @Template()
      */
     public function indexAction()
@@ -42,40 +42,9 @@ class ItemController extends Controller
     }
 
     /**
-     * one item page by id
-     * {sl} - for trailing slash if its needed
-     *
-     * @Route("/item/{id}{sl}" , name="item_page" , defaults={"sl":"","id":""} , requirements={"id":"[1-9][0-9]*", "sl":"/?"})
-     * @Template()
-     */
-    public function showAction($id) // or Request $request)
-    {
-//        $id = $request->get('id');
-
-        $item = $this->get('doctrine')->getRepository('AppBundle:Item')->find($id);
-
-        if (!$item) {
-            throw $this->createNotFoundException('Page not found!');
-        }
-
-//        dump($item);
-        $exporter = $this->get('text_export');
-        $exporter->export($item);
-
-//        dump($item);
-
-//        return compact('id');
-
-        return ['item'=>$item];
-
-//        return $this->render('item/show.html.twig',['id_item_for_twig' => $id]);
-//        return new Response("<html><body>item page : {$id} </body></html>");
-    }
-
-    /**
      * edit one item
      *
-     * @Route("/item/edit/{id}" , name="item_edit" , defaults={"id":""} , requirements={"id":"[1-9][0-9]*"})
+     * @Route("/admin/item/edit/{id}" , name="item_edit" , defaults={"id":""} , requirements={"id":"[1-9][0-9]*"})
      * @Template()
      */
     public function editAction(Request $request)
@@ -118,7 +87,7 @@ class ItemController extends Controller
     /**
      * add one new item
      *
-     * @Route("/item/add" , name="item_add")
+     * @Route("/admin/item/add" , name="item_add")
      * @Template()
      */
     public function addAction(Request $request)
@@ -143,7 +112,7 @@ class ItemController extends Controller
     /**
      * items list page
      *
-     * @Route("/item-test" , name="item_test_action")
+     * @Route("/admin/item-test" , name="item_test_action")
      * @Template()
      */
     public function testAction()
