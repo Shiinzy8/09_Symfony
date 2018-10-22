850c9ee61708d421eca3dd04e698870ebc078d88
Shiinzy8 <Shiinzy8@gmail.com>
2018-10-22 18:32:27 +0300

    docktrine, entity, migration

---
+++ src/Entity/MicroPost.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MicroPostRepository")
+ * @ORM\Table()
+ */
+class MicroPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * занчит это поле будет полем таблицы, если его не указать в таблицу это поле не попадет
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=280)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
