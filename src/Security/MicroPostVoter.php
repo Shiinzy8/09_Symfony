c7390eb550ec6ee8dfee7fbce23ad98e61e4649c
Shiinzy8 <Shiinzy8@gmail.com>
2018-10-31 18:19:19 +0200

    creating security voter

---
+++ src/Security/MicroPostVoter.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arkhyliuk
+ * Date: 31.10.18
+ * Time: 17:53
+ */
+
+namespace App\Security;
+
+
+use App\Entity\MicroPost;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class MicroPostVoter extends Voter
+{
+
+    const EDIT = 'edit';
+    const DELETE = 'delete';
+
+    // вызывается перед вызовом voteOnAttribute
+    /**
+     * Determines if the attribute and subject are supported by this voter.
+     *
+     * @param string $attribute An attribute
+     * @param mixed $subject The subject to secure, e.g. an object the user wants to access or any other PHP type
+     *
+     * @return bool True if the attribute and subject are supported, false otherwise
+     */
+    protected function supports($attribute, $subject)
+    {
+        if (!in_array($attribute, [self::EDIT, self::DELETE])) {
+            return false;
+        }
+
+        if (!$subject instanceof MicroPost) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Perform a single access check operation on a given attribute, subject and token.
+     * It is safe to assume that $attribute and $subject already passed the "supports()" method check.
+     *
+     * @param string $attribute
+     * @param mixed $subject
+     * @param TokenInterface $token
+     *
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $authenticatedUser = $token->getUser();
+
+        if (!$authenticatedUser instanceof User) {
+            return true;
+        }
+
+        // мы убедились что этот типа правильный в методе supports он вызывается первым
+        /** @var MicroPost $microPost */
+        $microPost = $subject;
+
+        if ($microPost->getUser()->getId() == $authenticatedUser->getId()) {
+            return true;
+        }
+
+        return false;
+    }
+}
