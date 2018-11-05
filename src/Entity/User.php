0cfd8203991b2b8b23aa5f9301fa9aba2fce02ae
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-05 17:24:26 +0200

    many to many relation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -99,6 +99,30 @@ class User implements UserInterface, \Serializable
      */
     private $roles;
 
+
+    /**
+     * Для коллекций не надо создавать сеттеры
+     *
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", mappedBy="following")
+     */
+    private $followers;
+
+
+    /**
+     * Для коллекций не надо создавать сеттеры
+     *
+     * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="followers")
+     * @ORM\JoinTable(name="following",
+     *     joinColumns={
+     *         @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     *     },
+     *     inverseJoinColumns={
+     *         @ORM\JoinColumn(name="following_user_id", referencedColumnName="id")
+     *     }
+     * )
+     */
+    private $following;
+
     /**
      * Doctrine doesn't do construct method it's only for our use
      * User constructor.
@@ -106,7 +130,8 @@ class User implements UserInterface, \Serializable
     public function __construct()
     {
         $this->posts = new ArrayCollection();
-        $this->roles = [$this::ROLE_USER];
+        $this->followers = new ArrayCollection();
+        $this->following = new ArrayCollection();
     }
 
     /**
@@ -203,6 +228,22 @@ class User implements UserInterface, \Serializable
     public function getPosts()
     {
         return $this->posts;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFollowers()
+    {
+        return $this->followers;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFollowing()
+    {
+        return $this->following;
     }
 
     /**
