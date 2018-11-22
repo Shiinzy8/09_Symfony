1f294c17f11ca964ff630c733ff0c789939aaecb
Shiinzy8 <Shiinzy8@gmail.com>
2018-11-22 12:36:54 +0200

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // directory where compiled assets will be stored
+// directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
@@ -29,16 +29,16 @@ Encore
 
     // .addEntry('js/app', './assets/js/app.js') // значения которые можно было использовать по умолчанию
     .addEntry('js/app', [
-        './node_modules/jquery/dist/jquery.slim.min.js',
-        './node_modules/popper.js/dist/popper.min.js',
-        './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/holderjs/holder.min.js',
-        './assets/js/app.js',
+            './node_modules/jquery/dist/jquery.slim.min.js',
+            './node_modules/popper.js/dist/popper.min.js',
+            './node_modules/bootstrap/dist/js/bootstrap.min.js',
+            './node_modules/holderjs/holder.min.js',
+            './assets/js/app.js',
         ]
     )
     .addStyleEntry('css/app', [
-        './node_modules/bootstrap/dist/css/bootstrap.min.css',
-        './assets/css/app.css',
+            './node_modules/bootstrap/dist/css/bootstrap.min.css',
+            './assets/css/app.css',
         ]
     )
     //.addEntry('page1', './assets/js/page1.js')
@@ -58,48 +58,48 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+// enables Sass/SCSS support
+//.enableSassLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 
-    .configureBabel((config) => {
-        config.plugins.push(
-            // Enable using dynamic imports, using modular per-import chunks
-            ['syntax-dynamic-import'],
-        );
-    })
+// .configureBabel((config) => {
+//     config.plugins.push(
+//         // Enable using dynamic imports, using modular per-import chunks
+//         ['syntax-dynamic-import'],
+//     );
+// })
 
-    // Presets
-    .configureBabel((config) => {
-        config.presets.push(
-            // Enable transpiling down to browser compatibility
-            ['env', { targets: { browsers: ['last 2 versions', 'safari >= 7'] } }],
-            // // Enable transpiling ES2015 to ES5 for uglifying non-ignored modules
-            // ['es2015'],
-        );
-    })
+// Presets
+// .configureBabel((config) => {
+//     config.presets.push(
+//         // Enable transpiling down to browser compatibility
+//         ['preset-env', { targets: { browsers: ['last 2 versions', 'safari >= 7'] } }],
+//         // // Enable transpiling ES2015 to ES5 for uglifying non-ignored modules
+//         // ['es2015'],
+//     );
+// })
 
-    // .configureBabel(function(babelConfig) {
-    //     // add additional presets
-    //     // babelConfig.presets.push('@babel/preset-flow');
-    //
-    //     babelConfig.presets.push('@babel/preset-env');
-    //
-    //     // no plugins are added by default, but you can add some
-    //     // babelConfig.plugins.push('styled-jsx/babel');
-    // }, {
-    //     // node_modules is not processed through Babel by default
-    //     // but you can whitelist specific modules to process
-    //     // include_node_modules: ['foundation-sites']
-    //
-    //     // or completely control the exclude
-    //     // exclude: /bower_components/
-    // })
+// .configureBabel(function(babelConfig) {
+//     // add additional presets
+//     // babelConfig.presets.push('@babel/preset-flow');
+//
+//     babelConfig.presets.push('@babel/preset-env');
+//
+//     // no plugins are added by default, but you can add some
+//     // babelConfig.plugins.push('styled-jsx/babel');
+// }, {
+//     // node_modules is not processed through Babel by default
+//     // but you can whitelist specific modules to process
+//     // include_node_modules: ['foundation-sites']
+//
+//     // or completely control the exclude
+//     // exclude: /bower_components/
+// })
 ;
 
 // export the final configuration
